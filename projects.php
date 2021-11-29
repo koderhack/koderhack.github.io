ffe211997a1c045bc7666d6d6719f7891edc939b
Koder123456 <59885735+koderhack@users.noreply.github.com>
2021-11-29 21:33:18 +0100

    page update

---
+++ projects.php
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Koder In YouTube</title>
+		<link rel="stylesheet" href="index.css">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="icon" href="favicon.ico">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"> 
+ <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	</head>
+	<body>
+<div id=menu> 	
+	<a href=index.php><video class="logo" autoplay loop muted>
+	<source src="video.mp4" type="video/mp4"></a>
+</video> 
+<div id=links style="display:flex">
+	
+		<a href=index.php class=links>Strona główna </a> 
+	    <a href="https://www.youtube.com/channel/UC37t0k39bcWRzAreu7bAkSQ/videos" class=links>Filmy </a>
+		<a href="#" class=links>Projekty </a>
+</div>
+
+<div id=linksphone class=linksphone>
+
+		<a href=index.php class=linksphoneelement>Strona główna</a> 
+
+		<a href="https://www.youtube.com/channel/UC37t0k39bcWRzAreu7bAkSQ/videos" class=linksphoneelement>Filmy</a>
+		<a href="#" class=linksphoneelement>Projekty</a>
+</div>
+
+
+</div>
+<div id=project1>
+Tutaj jeszcze nic nie ma<br>
+Strona się rozwija
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+<div id=stopka>
+</br>
+Wszelkie prawa zastrzeżone © Koder 2021</br>
+Email:<a href="mailto:koder1@interia.pl">koder1@interia.pl</a></br>
+Social Media:
+<a href="https://www.youtube.com/channel/UC37t0k39bcWRzAreu7bAkSQ" class="socials"><img src=yt.png class="socials"></a>
+<a href="https://discord.gg/twrMS8nUTZ" class="socials"><img src=discord.png class="socials"></a>
+<a href="https://twitter.com/Koder123456789" class="socials"><img src=twitter.png class="socials"></a>
+<a href="https://trello.com/koder123456" class="socials"><img src=trello.png class="socials"></a>
+<a href="https://www.tiktok.com/@koder1234567" class="socials"><img src=tiktok.png class="socials"></a>
+<a href="https://github.com/koderhack" class="socials"><img src=github.PNG class="socials"></a>
+</div>
+
+	</body>
+</html>
