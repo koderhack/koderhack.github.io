12d1fa8696c41e199920c8062ea97a2e84da4a2d
Koder123456 <59885735+koderhack@users.noreply.github.com>
2021-12-02 21:13:07 +0100

    Add files via upload

---
+++ i18n.js
@@ -0,0 +1,19 @@
+var translate = {
+    pl: {
+      linkstrona: "Strona główna",
+      linkfilmy: "Filmy",
+      linkprojekty: "Projekty",
+      kodertekst: "Jestem Koder.Lubię programować i prowadzę kanał na YouTube. &nbsp",
+      techtekst: "Technologie w których programuje:",
+      rights: "Wszelkie prawa zastrzeżone © Koder 2021"
+    },
+    en: {
+        linkstrona: "Main page",
+        linkfilmy: "Videos",
+        linkprojekty: "Projects",
+        kodertekst: "I am a Koder.I like programming and I have a channel on Youtube. &nbsp",
+        techtekst: "Technologies in which I program:",
+        rights: "All rights reserved © Koder 2021"
+    },
+    
+  };
